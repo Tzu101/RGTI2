1509fc4f9e4fab6f0c353d250493f2bfc05f3ad6
rokkondic <rok.kondic@gmail.com>
2023-01-07 19:27:59 +0100

    Added all the screens, level switching

--- Assets/Resources/Scripts/Global/Levels.cs
+++ Assets/Resources/Scripts/Global/Levels.cs
@@ -39,7 +39,273 @@ public class Level {
 
 public class Levels {
 
-    public static Level test = new Level(0,
+    public static Level l1 = new Level(0,
+        3, 3,
+        new Vector2(0, 0),
+        new int[,] { 
+            { 2, 2, 1 },
+            { 2, 2, 0 },
+            { 1, 0, 0 },
+        },
+        new int[,] { 
+            { 0, 0, 1 },
+            { 0, 2, 0 },
+            { 1, 0, 0 },
+        }
+    );
+    public static Level l2 = new Level(1,
+        3, 3,
+        new Vector2(0, 0),
+        new int[,] { 
+            { 2, 2, 1 },
+            { 2, 2, 0 },
+            { 1, 0, 0 },
+        },
+        new int[,] { 
+            { 0, 0, 1 },
+            { 0, 2, 0 },
+            { 1, 0, 0 },
+        }
+    );
+    public static Level l3 = new Level(2,
+        3, 3,
+        new Vector2(0, 0),
+        new int[,] { 
+            { 2, 2, 1 },
+            { 2, 2, 0 },
+            { 1, 0, 0 },
+        },
+        new int[,] { 
+            { 0, 0, 1 },
+            { 0, 2, 0 },
+            { 1, 0, 0 },
+        }
+    );
+    public static Level l4 = new Level(3,
+        3, 3,
+        new Vector2(0, 0),
+        new int[,] { 
+            { 2, 2, 1 },
+            { 2, 2, 0 },
+            { 1, 0, 0 },
+        },
+        new int[,] { 
+            { 0, 0, 1 },
+            { 0, 2, 0 },
+            { 1, 0, 0 },
+        }
+    );
+    public static Level l5 = new Level(4,
+        3, 3,
+        new Vector2(0, 0),
+        new int[,] { 
+            { 2, 2, 1 },
+            { 2, 2, 0 },
+            { 1, 0, 0 },
+        },
+        new int[,] { 
+            { 0, 0, 1 },
+            { 0, 2, 0 },
+            { 1, 0, 0 },
+        }
+    );
+    public static Level l6 = new Level(5,
+        3, 3,
+        new Vector2(0, 0),
+        new int[,] { 
+            { 2, 2, 1 },
+            { 2, 2, 0 },
+            { 1, 0, 0 },
+        },
+        new int[,] { 
+            { 0, 0, 1 },
+            { 0, 2, 0 },
+            { 1, 0, 0 },
+        }
+    );
+    public static Level l7 = new Level(6,
+        3, 3,
+        new Vector2(0, 0),
+        new int[,] { 
+            { 2, 2, 1 },
+            { 2, 2, 0 },
+            { 1, 0, 0 },
+        },
+        new int[,] { 
+            { 0, 0, 1 },
+            { 0, 2, 0 },
+            { 1, 0, 0 },
+        }
+    );
+    public static Level l8 = new Level(7,
+        3, 3,
+        new Vector2(0, 0),
+        new int[,] { 
+            { 2, 2, 1 },
+            { 2, 2, 0 },
+            { 1, 0, 0 },
+        },
+        new int[,] { 
+            { 0, 0, 1 },
+            { 0, 2, 0 },
+            { 1, 0, 0 },
+        }
+    );
+    public static Level l9 = new Level(8,
+        3, 3,
+        new Vector2(0, 0),
+        new int[,] { 
+            { 2, 2, 1 },
+            { 2, 2, 0 },
+            { 1, 0, 0 },
+        },
+        new int[,] { 
+            { 0, 0, 1 },
+            { 0, 2, 0 },
+            { 1, 0, 0 },
+        }
+    );
+    public static Level l10 = new Level(9,
+        3, 3,
+        new Vector2(0, 0),
+        new int[,] { 
+            { 2, 2, 1 },
+            { 2, 2, 0 },
+            { 1, 0, 0 },
+        },
+        new int[,] { 
+            { 0, 0, 1 },
+            { 0, 2, 0 },
+            { 1, 0, 0 },
+        }
+    );
+    public static Level l11 = new Level(10,
+        3, 3,
+        new Vector2(0, 0),
+        new int[,] { 
+            { 2, 2, 1 },
+            { 2, 2, 0 },
+            { 1, 0, 0 },
+        },
+        new int[,] { 
+            { 0, 0, 1 },
+            { 0, 2, 0 },
+            { 1, 0, 0 },
+        }
+    );
+    public static Level l12 = new Level(11,
+        3, 3,
+        new Vector2(0, 0),
+        new int[,] { 
+            { 2, 2, 1 },
+            { 2, 2, 0 },
+            { 1, 0, 0 },
+        },
+        new int[,] { 
+            { 0, 0, 1 },
+            { 0, 2, 0 },
+            { 1, 0, 0 },
+        }
+    );
+    public static Level l13 = new Level(12,
+        3, 3,
+        new Vector2(0, 0),
+        new int[,] { 
+            { 2, 2, 1 },
+            { 2, 2, 0 },
+            { 1, 0, 0 },
+        },
+        new int[,] { 
+            { 0, 0, 1 },
+            { 0, 2, 0 },
+            { 1, 0, 0 },
+        }
+    );
+    public static Level l14 = new Level(13,
+        3, 3,
+        new Vector2(0, 0),
+        new int[,] { 
+            { 2, 2, 1 },
+            { 2, 2, 0 },
+            { 1, 0, 0 },
+        },
+        new int[,] { 
+            { 0, 0, 1 },
+            { 0, 2, 0 },
+            { 1, 0, 0 },
+        }
+    );
+    public static Level l15 = new Level(14,
+        3, 3,
+        new Vector2(0, 0),
+        new int[,] { 
+            { 2, 2, 1 },
+            { 2, 2, 0 },
+            { 1, 0, 0 },
+        },
+        new int[,] { 
+            { 0, 0, 1 },
+            { 0, 2, 0 },
+            { 1, 0, 0 },
+        }
+    );
+    public static Level l16 = new Level(15,
+        3, 3,
+        new Vector2(0, 0),
+        new int[,] { 
+            { 2, 2, 1 },
+            { 2, 2, 0 },
+            { 1, 0, 0 },
+        },
+        new int[,] { 
+            { 0, 0, 1 },
+            { 0, 2, 0 },
+            { 1, 0, 0 },
+        }
+    );
+    public static Level l17 = new Level(16,
+        3, 3,
+        new Vector2(0, 0),
+        new int[,] { 
+            { 2, 2, 1 },
+            { 2, 2, 0 },
+            { 1, 0, 0 },
+        },
+        new int[,] { 
+            { 0, 0, 1 },
+            { 0, 2, 0 },
+            { 1, 0, 0 },
+        }
+    );
+    public static Level l18 = new Level(17,
+        3, 3,
+        new Vector2(0, 0),
+        new int[,] { 
+            { 2, 2, 1 },
+            { 2, 2, 0 },
+            { 1, 0, 0 },
+        },
+        new int[,] { 
+            { 0, 0, 1 },
+            { 0, 2, 0 },
+            { 1, 0, 0 },
+        }
+    );
+    public static Level l19 = new Level(18,
+        3, 3,
+        new Vector2(0, 0),
+        new int[,] { 
+            { 2, 2, 1 },
+            { 2, 2, 0 },
+            { 1, 0, 0 },
+        },
+        new int[,] { 
+            { 0, 0, 1 },
+            { 0, 2, 0 },
+            { 1, 0, 0 },
+        }
+    );
+    public static Level l20 = new Level(19,
         3, 3,
         new Vector2(0, 0),
         new int[,] { 
---
+++ Assets/Resources/Scripts/Managers/ButtonManager.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class ButtonManager : MonoBehaviour
+{
+    public void startGame(){
+        switch(EventSystem.current.currentSelectedGameObject.name.ToString()){
+            case "1":
+                Debug.Log(EventSystem.current.currentSelectedGameObject.name.ToString());
+                LevelManager.loadLevel(Levels.l1);
+            break;
+            case "2":
+                Debug.Log(EventSystem.current.currentSelectedGameObject.name.ToString());
+                LevelManager.loadLevel(Levels.l2);
+            break;
+            case "3":
+                LevelManager.loadLevel(Levels.l3);
+            break;
+            case "4":
+                LevelManager.loadLevel(Levels.l4);
+            break;
+            case "5":
+                LevelManager.loadLevel(Levels.l5);
+            break;
+            case "6":
+                Debug.Log(EventSystem.current.currentSelectedGameObject.name.ToString());
+                LevelManager.loadLevel(Levels.l6);
+            break;
+            case "7":
+                LevelManager.loadLevel(Levels.l7);
+            break;
+            case "8":
+                LevelManager.loadLevel(Levels.l8);
+            break;
+            case "9":
+                LevelManager.loadLevel(Levels.l9);
+            break;
+            case "10":
+                Debug.Log(EventSystem.current.currentSelectedGameObject.name.ToString());
+                LevelManager.loadLevel(Levels.l10);
+            break;
+            case "11":
+                LevelManager.loadLevel(Levels.l11);
+            break;
+            case "12":
+                LevelManager.loadLevel(Levels.l12);
+            break;
+            case "13":
+                LevelManager.loadLevel(Levels.l13);
+            break;
+            case "14":
+                LevelManager.loadLevel(Levels.l14);
+            break;
+            case "15":
+                LevelManager.loadLevel(Levels.l15);
+            break;
+            case "16":
+                LevelManager.loadLevel(Levels.l16);
+            break;
+            case "17":
+                LevelManager.loadLevel(Levels.l17);
+            break;
+            case "18":
+                LevelManager.loadLevel(Levels.l18);
+            break;
+            case "19":
+                LevelManager.loadLevel(Levels.l19);
+            break;
+            case "20":
+                LevelManager.loadLevel(Levels.l20);
+            break;
+        }      
+    }
+    public void levelSelect(){
+        SceneManager.LoadScene("LevelSelect");        
+    }
+    public void newStuff(){
+        SceneManager.LoadScene("New"); 
+    }
+    public void instructions(){
+        SceneManager.LoadScene("Help"); 
+    }
+    public void backToMain(){
+        SceneManager.LoadScene("Title");         
+    }
+    public void exit(){
+        Application.Quit();
+    }
+}
+
